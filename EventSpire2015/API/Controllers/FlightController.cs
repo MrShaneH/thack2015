53bb1cdf8a10aafba03b7e063147bd34042ac4eb
theglennons <kglennon+theglennons@gmail.com>
2015-05-09 16:38:33 +0100

    no message

---
+++ EventSpire2015/API/Controllers/FlightController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class FlightController : Controller
+    {
+        // POST api/values
+        [HttpGet]
+        public string Get(string longitude, string latitude, string radius, string startDate)
+        {
+            string json = System.IO.File.ReadAllText(@"C:\code\thack2015\JSONSamples\FlightScheduleResponse.json");
+            return json;
+        }
+
+    }
+}
---
+++ EventSpire2015/API/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class SearchController : Controller
+    {
+        // GET http://localhost:50362/api/search?userLongitude=1&userLatitude=2&eventCategory=3
+        [HttpGet]
+        public string Get(string userLongitude, string userLatitude, string eventCategory)
+        {
+            string json = System.IO.File.ReadAllText(@"C:\code\thack2015\JSONSamples\FlightScheduleRequest.json");
+            return json;
+        }
+        
+    }
+}
